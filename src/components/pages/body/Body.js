f3446915a3d36facd0ad47f482a1d975e94695c3
Borhan Uddin <borhaninfos@gmail.com>
2022-03-23 01:30:03 +0600

    Update Body.js

--- src/components/pages/body/Body.js
+++ src/components/pages/body/Body.js
@@ -2,6 +2,27 @@ import React, { Component } from "react";
 import Profile from "./Profile";
 
 export default class Body extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      profile: null,
+      repos: null,
+    };
+  }
+
+  updateInput = (e) => {
+    this.setState({
+      ...this.state,
+      username: e.target.value,
+    });
+  };
+
+  //Form Submit
+  searchUser = (event) => {
+    event.preventDefault();
+  };
+
   render() {
     return (
       <main>
@@ -12,18 +33,22 @@ export default class Body extends Component {
                 <h1>Search Github User's</h1>
               </div>
               <div className="col-lg-6">
-                <input
-                  type="text"
-                  className="form-control"
-                  id="searchUser"
-                  placeholder="Github Username....."
-                />
-                <input
-                  type="submit"
-                  id="searchBtn"
-                  className="btn btn-success"
-                  value="Search"
-                />
+                <form className="form-inline" onSubmit={this.searchUser}>
+                  <input
+                    onChange={this.updateInput}
+                    value={this.state.username}
+                    type="text"
+                    className="form-control"
+                    id="searchUser"
+                    placeholder="Github Username....."
+                  />
+                  <input
+                    type="submit"
+                    id="searchBtn"
+                    className="btn btn-success"
+                    value="Search"
+                  />
+                </form>
               </div>
             </div>
           </div>
