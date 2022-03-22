1e3408e3c5f767286bd6f37aa2b66834bfcedd40
Borhan Uddin <borhaninfos@gmail.com>
2022-03-23 00:09:37 +0600

    bootstrap add

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function MainComponent() {
-  return <div>MainComponent</div>;
+  return (
+    <div className="mt-5 d-flex justify-content-center">MainComponent</div>
+  );
 }
